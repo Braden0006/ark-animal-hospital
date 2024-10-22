c6c679bae4bf414bb0ff02465a5f8d52e3500db5
Braden0006 <bradenwhitch@gmail.com>
2024-10-22 15:52:21 -0600

    Implemented layout for navigation bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'navigation-bg': '#2D2D2D',
+      }
+    },
   },
   plugins: [],
 }
