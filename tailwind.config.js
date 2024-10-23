e74e820a65b1b8a9258f2f119f1383724e87351a
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2024-10-22 20:54:46 -0600

    Implemented mobile layout for about page

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,15 @@ module.exports = {
       colors: {
         'navigation-bg': '#2D2D2D',
         'button-bg': '#008080',
+        'paragraph-color': '#333333',
       },
       fontSize: {
         '2.5rem': ['2.5rem', {
           lineHeight: '3rem',
         }],
+      },
+      lineHeight: {
+        'loosey-goosey': '5rem',
       }
     },
   },
