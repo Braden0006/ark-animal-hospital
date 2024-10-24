dce3bf1b05f725447b567faa63e42096739d9e27
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2024-10-23 18:50:35 -0600

    Implemented Annual Checkup page layout for mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         'navigation-bg': '#2D2D2D',
         'button-bg': '#008080',
         'paragraph-color': '#333333',
+        'service-card-bg': '#2D2D2D',
       },
       fontSize: {
         '2.5rem': ['2.5rem', {
@@ -15,11 +16,15 @@ module.exports = {
       },
       lineHeight: {
         'loosey-goosey': '5rem',
+        '4rem': '4rem',
       },
       height: {
         '26': '26rem',
         '68': '17rem',
         '70': '17.5rem',
+      },
+      maxWidth: {
+        '52rem': '52rem',
       }
     },
   },
