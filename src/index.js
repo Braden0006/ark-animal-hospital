6380886caad902b75689ab4120171a1836929ac0
Braden0006 <bradenwhitch@gmail.com>
2024-11-03 06:46:31 -0700

    Modal content changes depending on which card is clicked

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
 let modalBackground = document.getElementById("modal_background");
 let modal = document.getElementById("modal");
-let card = document.getElementById("card");
+let card1 = document.getElementById("card1");
+let card2 = document.getElementById("card2");
+let card3 = document.getElementById("card3");
 let body = document.getElementById("body");
+let modalTitle = document.getElementById("modal_title");
+let modalPara = document.getElementById("modal_para");
 
 function cardClicked() {
     modalBackground.classList.add("show_modal_background");
     modal.classList.add("show_modal");
     body.classList.add("stop_scrolling");
+
+    modalTitle.innerHTML = "Sam";
+    modalPara.innerHTML = "" +
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n" +
+        "Quisque consequat, justo nec gravida auctor, sem velit vehicula\n" +
+        "augue, id rhoncus lectus elit vel elit. Sed accumsan fermentum\n" +
+        "sem, vel fringilla lacus pulvinar non."
+}
+
+function card2Clicked() {
+    modalBackground.classList.add("show_modal_background");
+    modal.classList.add("show_modal");
+    body.classList.add("stop_scrolling");
+
+    modalTitle.innerHTML = "Andrew";
+    modalPara.innerHTML = "" +
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n" +
+        "Quisque consequat, justo nec gravida auctor, sem velit vehicula\n" +
+        "augue, id rhoncus lectus elit vel elit. Sed accumsan fermentum\n" +
+        "sem, vel fringilla lacus pulvinar non."
+}
+
+function card3Clicked() {
+    modalBackground.classList.add("show_modal_background");
+    modal.classList.add("show_modal");
+    body.classList.add("stop_scrolling");
+
+    modalTitle.innerHTML = "Allison";
+    modalPara.innerHTML = "" +
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n" +
+        "Quisque consequat, justo nec gravida auctor, sem velit vehicula\n" +
+        "augue, id rhoncus lectus elit vel elit. Sed accumsan fermentum\n" +
+        "sem, vel fringilla lacus pulvinar non."
 }
 
 function hideModal() {
@@ -15,5 +52,17 @@ function hideModal() {
     body.classList.remove("stop_scrolling");
 }
 
-card.addEventListener("click", cardClicked);
+card1.addEventListener("click", cardClicked);
 modal.addEventListener("click", hideModal);
+
+if (card1.addEventListener) {
+    card1.addEventListener("click", cardClicked);
+}
+
+if (card2.addEventListener) {
+    card2.addEventListener("click", card2Clicked);
+}
+
+if (card3.addEventListener) {
+    card3.addEventListener("click", card3Clicked);
+}
