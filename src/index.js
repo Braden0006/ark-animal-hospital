d77f6795599a8238cbc0554e8353b8e91e87fff8
Braden0006 <bradenwhitch@gmail.com>
2024-11-03 05:54:24 -0700

    Scrolling is disabled when the modal is shown

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 let modalBackground = document.getElementById("modal_background");
 let modal = document.getElementById("modal");
 let card = document.getElementById("card");
+let body = document.getElementById("body");
 
 function cardClicked() {
     modalBackground.classList.add("show_modal_background");
     modal.classList.add("show_modal");
+    body.classList.add("stop_scrolling");
 }
 
 function hideModal() {
     modalBackground.classList.remove("show_modal_background");
     modal.classList.remove("show_modal");
+    body.classList.remove("stop_scrolling");
 }
 
 card.addEventListener("click", cardClicked);
