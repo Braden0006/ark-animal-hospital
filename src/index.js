663d0081a21746765b30189cd48f2494078dce16
Braden0006 <bradenwhitch@gmail.com>
2024-11-09 07:53:01 -0700

    Implemented dropdown menu for mobile and tablet

--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ let body = document.getElementById("body");
 let modalTitle = document.getElementById("modal_title");
 let modalPara = document.getElementById("modal_para");
 
+let bar1 = document.getElementById("bar-1");
+let bar2 = document.getElementById("bar-2");
+let bar3 = document.getElementById("bar-3");
+let menu = document.getElementById("ham-menu");
+let dropdownMenu = document.getElementById("dropdown-menu");
+
+let mobileLink = document.getElementsByClassName("navbar_mobile_dropdown_link");
+
 function cardClicked() {
     modalBackground.classList.add("show_modal_background");
     modal.classList.add("show_modal");
@@ -65,4 +73,26 @@ if (card2.addEventListener) {
 
 if (card3.addEventListener) {
     card3.addEventListener("click", card3Clicked);
+}
+
+menu.addEventListener("click", () => {
+    body.classList.toggle("stop_scrolling");
+
+    bar1.classList.toggle("bar-1");
+    bar2.classList.toggle("bar-2");
+    bar3.classList.toggle("bar-3");
+
+    dropdownMenu.classList.toggle("navbar_mobile_dropdown_show");
+});
+
+for (let i = 0; i < mobileLink.length; i++) {
+    mobileLink[i].addEventListener("click", () => {
+        body.classList.toggle("stop_scrolling");
+
+        bar1.classList.toggle("bar-1");
+        bar2.classList.toggle("bar-2");
+        bar3.classList.toggle("bar-3");
+
+        dropdownMenu.classList.toggle("navbar_mobile_dropdown_show");
+    })
 }
