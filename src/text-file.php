b0f34fb2e22ddeca1155fd6fd7e1c2ea3510b9a4
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2024-11-29 08:15:03 -0700

    Implemented basic layout and content for new client form

--- src/text-file.php
+++ src/text-file.php
@@ -11,11 +11,74 @@ $firstName = $_POST['first_name'];
 $middleInitial = $_POST['middle_initial'];
 $lastName = $_POST['last_name'];
 
+$spouseFirstName = $_POST['spouse_first_name'];
+$spouseMiddleInitial = $_POST['spouse_middle_initial'];
+$spouseLastName = $_POST['spouse_last_name'];
+
+$physicalAddress = $_POST['physical_address'];
+$city = $_POST['city'];
+$state = $_POST['state'];
+$zip = $_POST['zip'];
+
+$mailingAddress = $_POST['mailing_address-2'];
+$city2 = $_POST['city-2'];
+$state2 = $_POST['state-2'];
+$zip2 = $_POST['zip-2'];
+
+$phone = $_POST['phone'];
+$cell = $_POST['cell'];
+$spousePhone = $_POST['spouse_phone'];
+
+$email = $_POST['email'];
+$employment = $_POST['employment'];
+$phone2 = $_POST['phone-2'];
+
+$petName = $_POST['pet_name'];
+$petAge = $_POST['pet_age'];
+$species = $_POST['species'];
+$breed = $_POST['breed'];
+$color = $_POST['pet_color'];
+
+$male = $_POST['male-female'];
+$female = $_POST['male-female'];
+$spayed = $_POST['spayed-neutered'];
+$neutered = $_POST['spayed-neutered'];
+
 $filePath = 'data.txt';
 
 $data = "First Name: $firstName
 Middle Initial: $middleInitial
-Last Name: $lastName";
+Last Name: $lastName
+
+Spouse First Name: $spouseFirstName
+Spouse Middle Initial: $spouseMiddleInitial
+Spouse Last Name: $spouseLastName
+
+Physical Address: $physicalAddress
+City: $city
+State: $state
+Zip: $zip
+
+Mailing Address: $mailingAddress
+City2: $city2
+State2: $state2
+Zip2: $zip2
+
+Phone: $phone
+Cell: $cell
+Spouses Phone: $spousePhone
+
+Pet Name: $petName
+Pet Age: $petAge
+Species: $species
+Breed: $breed
+Color: $color
+
+Sex:
+$male
+$female
+$spayed
+$neutered";
 
 $fileHandle = fopen($filePath, 'a');
 ftruncate($fileHandle, 0);
