0f3162e2d4a29bd4e5723be5e00cae874e7b921c
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2024-12-27 07:50:28 -0700

    Added styling to new client form

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 let modalBackground = document.getElementById("modal_background");
 let modal = document.getElementById("modal");
-let card1 = document.getElementById("card1");
+let modal2 = document.getElementById("modal2");
 let card2 = document.getElementById("card2");
 let card3 = document.getElementById("card3");
 let body = document.getElementById("body");
@@ -19,26 +19,12 @@ function cardClicked() {
     modalBackground.classList.add("show_modal_background");
     modal.classList.add("show_modal");
     body.classList.add("stop_scrolling");
-
-    modalTitle.innerHTML = "Sam";
-    modalPara.innerHTML = "" +
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n" +
-        "Quisque consequat, justo nec gravida auctor, sem velit vehicula\n" +
-        "augue, id rhoncus lectus elit vel elit. Sed accumsan fermentum\n" +
-        "sem, vel fringilla lacus pulvinar non."
 }
 
 function card2Clicked() {
     modalBackground.classList.add("show_modal_background");
-    modal.classList.add("show_modal");
+    modal2.classList.add("show_modal");
     body.classList.add("stop_scrolling");
-
-    modalTitle.innerHTML = "Andrew";
-    modalPara.innerHTML = "" +
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n" +
-        "Quisque consequat, justo nec gravida auctor, sem velit vehicula\n" +
-        "augue, id rhoncus lectus elit vel elit. Sed accumsan fermentum\n" +
-        "sem, vel fringilla lacus pulvinar non."
 }
 
 function card3Clicked() {
@@ -60,11 +46,15 @@ function hideModal() {
     body.classList.remove("stop_scrolling");
 }
 
-card1.addEventListener("click", cardClicked);
-modal.addEventListener("click", hideModal);
+//modal.addEventListener("click", cardClicked);
+//modal.addEventListener("click", hideModal);
 
-if (card1.addEventListener) {
-    card1.addEventListener("click", cardClicked);
+if (modal.addEventListener) {
+    modal.addEventListener("click", cardClicked)
+}
+
+if (modal2.addEventListener) {
+    modal2.addEventListener("click", cardClicked);
 }
 
 if (card2.addEventListener) {
