9a5fc6bb0aeb42f7fff0ab726184b117c2b52c4c
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2025-01-09 00:15:28 -0700

    Added more security and input validation to the new client form

--- src/text-file.php
+++ src/text-file.php
@@ -7,158 +7,270 @@ require "../vendor/autoload.php";
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 
-$firstName = $_POST['first_name'];
-$middleInitial = $_POST['middle_initial'];
-$lastName = $_POST['last_name'];
+$firstName = isset($_POST['first_name']) ? htmlspecialchars($_POST['first_name']) : "";
+$middleInitial = isset($_POST['middle_initial']) ? htmlspecialchars($_POST['middle_initial']) : "";
+$lastName = isset($_POST['last_name']) ? htmlspecialchars($_POST['last_name']) : "";
 
-$spouseFirstName = $_POST['spouse_first_name'];
-$spouseMiddleInitial = $_POST['spouse_middle_initial'];
-$spouseLastName = $_POST['spouse_last_name'];
+$spouseFirstName = isset($_POST['spouse_first_name']) ? htmlspecialchars($_POST['spouse_first_name']) : "";
+$spouseMiddleInitial = isset($_POST['spouse_middle_initial']) ? htmlspecialchars($_POST['spouse_middle_initial']) : "";
+$spouseLastName = isset($_POST['spouse_last_name']) ? htmlspecialchars($_POST['spouse_last_name']) : "";
 
-$physicalAddress = $_POST['physical_address'];
-$city = $_POST['city'];
-$state = $_POST['state'];
-$zip = $_POST['zip'];
+$physicalAddress = isset($_POST['physical_address']) ? htmlspecialchars($_POST['physical_address']) : "";
+$city = isset($_POST['city']) ? htmlspecialchars($_POST['city']) : "";
+$state = isset($_POST['state']) ? htmlspecialchars($_POST['state']) : "";
+$zip = isset($_POST['zip']) ? htmlspecialchars($_POST['zip']) : "";
 
-$mailingAddressYes = $_POST['yes-no'];
-$mailingAddressNo = $_POST['yes-no'];
+$mailingAddressYesNo = $_POST['yes-no'];
 
-$mailingAddress = $_POST['mailing_address-2'];
-$city2 = $_POST['city-2'];
-$state2 = $_POST['state-2'];
-$zip2 = $_POST['zip-2'];
+$mailingAddress = isset($_POST['mailing_address-2']) ? htmlspecialchars($_POST['mailing_address-2']) : "";
+$city2 = isset($_POST['city-2']) ? htmlspecialchars($_POST['city-2']) : "";
+$state2 = isset($_POST['state-2']) ? htmlspecialchars($_POST['state-2']) : "";
+$zip2 = isset($_POST['zip-2']) ? htmlspecialchars($_POST['zip-2']) : "";
 
-$phone = $_POST['phone'];
-$cell = $_POST['cell'];
-$spousePhone = $_POST['spouse_phone'];
+$phone = isset($_POST['phone']) ? htmlspecialchars($_POST['phone']) : "";
+$cell = isset($_POST['cell']) ? htmlspecialchars($_POST['cell']) : "";
+$spousePhone = isset($_POST['spouse_phone']) ? htmlspecialchars($_POST['spouse_phone']) : "";
 
-$email = $_POST['email'];
-$employment = $_POST['employment'];
-$phone2 = $_POST['phone-2'];
+$email = isset($_POST['email']) ? htmlspecialchars($_POST['email']) : "";
+$employment = isset($_POST['employment']) ? htmlspecialchars($_POST['employment']) : "";
+$phone2 = isset($_POST['phone-2']) ? htmlspecialchars($_POST['phone-2']) : "";
 
-$petName = $_POST['pet_name'];
-$petAge = $_POST['pet_age'];
-$species = $_POST['species'];
-$breed = $_POST['breed'];
-$color = $_POST['pet_color'];
+$petName = isset($_POST['pet_name']) ? htmlspecialchars($_POST['pet_name']) : "";
+$petAge = isset($_POST['pet_age']) ? htmlspecialchars($_POST['pet_age']) : "";
+$species = isset($_POST['species']) ? htmlspecialchars($_POST['species']) : "";
+$breed = isset($_POST['breed']) ? htmlspecialchars($_POST['breed']) : "";
+$color = isset($_POST['pet_color']) ? htmlspecialchars($_POST['pet_color']) : "";
 
-$male = $_POST['male-female'];
-$female = $_POST['male-female'];
-$spayed = $_POST['spayed-neutered'];
-$neutered = $_POST['spayed-neutered'];
+$gender = $_POST['male-female'];
+$spayedOrNeutered = $_POST['spayed-neutered'];
 
-$petName2 = $_POST['pet_name2'];
-$petAge2 = $_POST['pet_age2'];
-$species2 = $_POST['species2'];
-$breed2 = $_POST['breed2'];
-$color2 = $_POST['pet_color2'];
+$petName2 = isset($_POST['pet_name2']) ? htmlspecialchars($_POST['pet_name2']) : "";
+$petAge2 = isset($_POST['pet_age2']) ? htmlspecialchars($_POST['pet_age2']) : "";
+$species2 = isset($_POST['species2']) ? htmlspecialchars($_POST['species2']) : "";
+$breed2 = isset($_POST['breed2']) ? htmlspecialchars($_POST['breed2']) : "";
+$color2 = isset($_POST['pet_color2']) ? htmlspecialchars($_POST['pet_color2']) : "";
 
-$male2 = $_POST['male-female2'];
-$female2 = $_POST['male-female2'];
-$spayed2 = $_POST['spayed-neutered2'];
-$neutered2 = $_POST['spayed-neutered2'];
+$gender2 = $_POST['male-female2'];
+$spayedOrNeutered2 = $_POST['spayed-neutered2'];
 
-$petName3 = $_POST['pet_name3'];
-$petAge3 = $_POST['pet_age3'];
-$species3 = $_POST['species3'];
-$breed3 = $_POST['breed3'];
-$color3 = $_POST['pet_color3'];
+$petName3 = isset($_POST['pet_name3']) ? htmlspecialchars($_POST['pet_name3']) : "";
+$petAge3 = isset($_POST['pet_age3']) ? htmlspecialchars($_POST['pet_age3']) : "";
+$species3 = isset($_POST['species3']) ? htmlspecialchars($_POST['species3']) : "";
+$breed3 = isset($_POST['breed3']) ? htmlspecialchars($_POST['breed3']) : "";
+$color3 = isset($_POST['pet_color3']) ? htmlspecialchars($_POST['pet_color3']) : "";
 
-$male3 = $_POST['male-female3'];
-$female3 = $_POST['male-female3'];
-$spayed3 = $_POST['spayed-neutered3'];
-$neutered3 = $_POST['spayed-neutered3'];
+$gender3 = $_POST['male-female3'];
+$spayedOrNeutered3 = $_POST['spayed-neutered3'];
 
-$petName4 = $_POST['pet_name4'];
-$petAge4 = $_POST['pet_age4'];
-$species4 = $_POST['species4'];
-$breed4 = $_POST['breed4'];
-$color4 = $_POST['pet_color4'];
+$petName4 = isset($_POST['pet_name4']) ? htmlspecialchars($_POST['pet_name4']) : "";
+$petAge4 = isset($_POST['pet_age4']) ? htmlspecialchars($_POST['pet_age4']) : "";
+$species4 = isset($_POST['species4']) ? htmlspecialchars($_POST['species4']) : "";
+$breed4 = isset($_POST['breed4']) ? htmlspecialchars($_POST['breed4']) : "";
+$color4 = isset($_POST['pet_color4']) ? htmlspecialchars($_POST['pet_color4']) : "";
 
-$male4 = $_POST['male-female4'];
-$female4 = $_POST['male-female4'];
-$spayed4 = $_POST['spayed-neutered4'];
-$neutered4 = $_POST['spayed-neutered4'];
+$gender4 = $_POST['male-female4'];
+$spayedOrNeutered4 = $_POST['spayed-neutered4'];
 
 $filePath = 'new-client.txt';
 
-$data = "First Name: $firstName
-Middle Initial: $middleInitial
-Last Name: $lastName
+$clientData = [
+    'Name' => $firstName . " " . $middleInitial . " " . $lastName,
+    'Spouse Name' => $spouseFirstName . " " . $spouseMiddleInitial . " " . $spouseLastName,
+    'Address' => $physicalAddress,
+    'City' => $city,
+    'State' => $state,
+    'Zip' => $zip,
+    'Mailing address the same as physical address?' => $mailingAddressYesNo,
 
-Spouse First Name: $spouseFirstName
-Spouse Middle Initial: $spouseMiddleInitial
-Spouse Last Name: $spouseLastName
+    'Mailing Address' => $mailingAddress,
+    'City2' => $city2,
+    'State2' => $state2,
+    'Zip2' => $zip2,
 
-Physical Address: $physicalAddress
-City: $city
-State: $state
-Zip: $zip
+    'Phone' => $phone,
+    'Cell' => $cell,
+    'Spouses Phone' => $spousePhone,
+    'Email' => $email,
+    'Employment' => $employment,
+    'Employment Phone' => $phone2,
+];
 
-Mailing address the same as physical address?: 
-$mailingAddressYes
-$mailingAddressNo
+$petData = [
+    'Pet Name' => $petName,
+    'Pet Age' => $petAge,
+    'Species' => $species,
+    'Breed' => $breed,
+    'Color' => $color,
+    'Sex' => $gender . " " . $spayedOrNeutered,
+];
 
-Mailing Address: $mailingAddress
-City2: $city2
-State2: $state2
-Zip2: $zip2
+$data = "Client Info:\n\n";
+if (!empty($firstName)) {
+    $data .= "First Name: $firstName\n";
+}
 
-Phone: $phone
-Cell: $cell
-Spouses Phone: $spousePhone
+if (!empty($middleInitial)) {
+    $data .= "Middle Initial: $middleInitial\n";
+}
 
-Email: $email
-Employment: $employment
-Phone: $phone2
+if (!empty($lastName)) {
+    $data .= "Last Name: $lastName\n";
+}
 
-Pet Name: $petName
-Pet Age: $petAge
-Species: $species
-Breed: $breed
-Color: $color
-Sex:
-$male
-$female
-$spayed
-$neutered
+if (!empty($spouseFirstName)) {
+    $data .= "Spouse First Name: $spouseFirstName\n";
+}
 
-Pet Name 2: $petName2
-Pet Age 2: $petAge2
-Species 2: $species2
-Breed 2: $breed2
-Color 2: $color2
-Sex:
-$male2
-$female2
-$spayed2
-$neutered2
+if (!empty($spouseMiddleInitial)) {
+    $data .= "Spouse Middle Initial: $spouseMiddleInitial\n";
+}
 
-Pet Name 3: $petName3
-Pet Age 3: $petAge3
-Species 3: $species3
-Breed 3: $breed3
-Color 3: $color3
-Sex:
-$male3
-$female3
-$spayed3
-$neutered3
+if (!empty($spouseLastName)) {
+    $data .= "Spouse Last Name: $spouseLastName\n";
+}
 
-Pet Name 4: $petName4
-Pet Age 4: $petAge4
-Species 4: $species4
-Breed 4: $breed4
-Color 4: $color4
-Sex:
-$male4
-$female4
-$spayed4
-$neutered4";
+if (!empty($physicalAddress)) {
+    $data .= "Address: $physicalAddress\n";
+}
 
-$fileHandle = fopen($filePath, 'a');
-ftruncate($fileHandle, 0);
-fclose($fileHandle);
+if (!empty($city)) {
+    $data .= "City: $city\n";
+}
+
+if (!empty($state)) {
+    $data .= "State: $state\n";
+}
+
+if (!empty($zip)) {
+    $data .= "Zip: $zip\n";
+}
+
+if (!empty($mailingAddressYesNo)) {
+    $data .= "Mailing Address the same as physical address?: $mailingAddressYesNo\n";
+}
+
+if (!empty($mailingAddress)) {
+    $data .= "Mailing Address: $mailingAddress\n";
+}
+
+if (!empty($city2)) {
+    $data .= "City2: $city2\n";
+}
+
+if (!empty($state2)) {
+    $data .= "State2: $state2\n";
+}
+
+if (!empty($zip2)) {
+    $data .= "Zip2: $zip2\n";
+}
+
+if (!empty($phone)) {
+    $data .= "Phone: $phone\n";
+}
+
+if (!empty($cell)) {
+    $data .= "Cell: $cell\n";
+}
+
+if (!empty($spousePhone)) {
+    $data .= "Spouse Phone: $spousePhone\n";
+}
+
+if (!empty($email)) {
+    $data .= "Email: $email\n";
+}
+
+if (!empty($employment)) {
+    $data .= "Employment: $employment\n";
+}
+
+if (!empty($phone2)) {
+    $data .= "Employment Phone: $phone2\n";
+}
+
+$data .= "\n\n\n\nPet Info:\n\n";
+foreach ($petData as $key => $value) {
+    $data .= "$key: $value\n";
+}
+
+$data .= "\n";
+
+if (!empty($petName2)) {
+    $data .= "Second Pet Name: $petName2\n";
+
+    if (!empty($petAge2)) {
+        $data .= "Second Pet Age: $petAge2\n";
+    }
+
+    if (!empty($species2)) {
+        $data .= "Species: $species2\n";
+    }
+
+    if (!empty($breed2)) {
+        $data .= "Breed: $breed2\n";
+    }
+
+    if (!empty($color2)) {
+        $data .= "Color: $color2\n";
+    }
+
+    if (!empty($gender2)) {
+        $data .= "Sex: " . $gender2 . " " . $spayedOrNeutered2 . "\n";
+    }
+}
+
+$data .= "\n";
+
+if (!empty($petName3)) {
+    $data .= "Third Pet Name: $petName3\n";
+
+    if (!empty($petAge3)) {
+        $data .= "Third Pet Age: $petAge3\n";
+    }
+
+    if (!empty($species3)) {
+        $data .= "Species: $species3\n";
+    }
+
+    if (!empty($breed3)) {
+        $data .= "Breed: $breed3\n";
+    }
+
+    if (!empty($color3)) {
+        $data .= "Color: $color3\n";
+    }
+
+    if (!empty($gender3)) {
+        $data .= "Sex: " . $gender3 . " " . $spayedOrNeutered3 . "\n";
+    }
+}
+
+$data .= "\n";
+
+if (!empty($petName4)) {
+    $data .= "Fourth Pet Name: $petName4\n";
+
+    if (!empty($petAge4)) {
+        $data .= "Fourth Pet Age: $petAge4\n";
+    }
+
+    if (!empty($species4)) {
+        $data .= "Species: $species4\n";
+    }
+
+    if (!empty($breed4)) {
+        $data .= "Breed: $breed4\n";
+    }
+
+    if (!empty($color4)) {
+        $data .= "Color: $color4\n";
+    }
+
+    if (!empty($gender4)) {
+        $data .= "Sex: " . $gender4 . " " . $spayedOrNeutered4 . "\n";
+    }
+}
 
 $fileHandle = fopen($filePath, 'a');
 fwrite($fileHandle, $data);
